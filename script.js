65fbf4beec357a064e0206c365898ad5514d7d8b
dtdarpan21-gif <dtdarpan21@gmail.com>
2026-09-24 17:53:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //your JS code here. If required.
-const status = document.getElementByid("Status");
-const enterbtn = document.getElementByid("enterbtn");
+const status = document.getElementById("Status");
+const enterBtn = document.getElementById("enterbtn");
 
 entrBtn.addEventListener("click",function(){
 	status.innerHtml = "<h1>Entered Metaverse</h1>";
